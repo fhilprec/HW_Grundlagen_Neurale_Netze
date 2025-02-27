01acd8beaa7d9f4f9984268ce63037ffe6a30fe8
Hardware for AI Praktikum <hw4ai01@einsilbix.ies.e-technik.tu-darmstadt.de>
2025-02-27 13:30:13 +0100

    a

--- lab5/synthesis.tcl
+++ lab5/synthesis.tcl
@@ -1,10 +1,7 @@
 set target_library NangateOpenCellLibrary_typical.db ; # tell synopsis which library to use
 set link_library NangateOpenCellLibrary_typical.db
-
-read_file mul.v ; # read specified input file
-
+read_file neuron.v ; # read specified input file
 link ; # link design with library
 compile_ultra ; # compile design with extra effort
-
-write -f verilog -hierarchy -output netlist_mul.v ; # save result as verilog netlist
+write -f verilog -hierarchy -output netlist_neuron.v ; # save result as verilog netlist
 quit
--- lab5/testbench.tcl
+++ lab5/testbench.tcl
@@ -1,8 +1,8 @@
 vlib work ; # create library work
 
-vlog testbench_mul.v ; # add file to library
-vlog mul.v
-vsim work.testbench_mul ; # simulate module testbench_add in lib work
+vlog testbench_neuron.v ; # add file to library
+vlog neuron.v
+vsim work.testbench_neuron ; # simulate module testbench_add in lib work
 
 run 10 us ; # run simulation for 10 us
 quit -f
